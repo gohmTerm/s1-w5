65838062d89fe29bdf2a49144e8047928a5dbd9c
gohmTerm <141902908+gohmTerm@users.noreply.github.com>
2026-09-09 11:37:33 -0700

    Sept9 - 2nd

--- WordGuessing.java
+++ WordGuessing.java
@@ -4,15 +4,16 @@ public class WordGuessing {
   
     //we will say that secret must be five characters
     public WordGuessing(String secret) {
-  
+      wordToBeGuessed = secret;
     }
   
     public void update(String letter) {
-  
+      int letPosition = wordToBeGuessed.indexOf(letter);
+      userString = userString.substring(0,letPosition) + letter + userString.substring(letPosition+1);
     }
   
     public String getUserString(){
-      
+      return userString;
     }
   }
   
--- WordGuessingMain.java
+++ WordGuessingMain.java
@@ -1,5 +1,7 @@
 public class WordGuessingMain{
     public static void main(String[] args){
-        
+        WordGuessing w = new WordGuessing("witch");
+        w.update("c");
+        System.out.println(w.getUserString);
     }
 }
